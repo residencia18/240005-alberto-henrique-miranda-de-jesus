a8fb8effe021546f43dd68bc1a067512f5c28231
albertolunia <albertolunia@gmail.com>
2024-03-12 15:37:53 -0300

    Adicionando validadores de data e hora

--- Exercicio2/Data.cs
+++ Exercicio2/Data.cs
@@ -18,6 +18,10 @@ namespace Exercicio2
 
         public Data(int dia, int mes, int ano)
         {
+            if (!DataValida(dia, mes, ano))
+            {
+                return;
+            }
             this.dia = dia;
             this.mes = mes;
             this.ano = ano;
@@ -25,7 +29,7 @@ namespace Exercicio2
 
         public Data(int dia, int mes, int ano, int hora, int minuto, int segundo) : this(dia, mes, ano)
         {
-            if (hora < 0 || hora > 23)
+            if (!HoraValida(hora, minuto, segundo))
             {
                 return;
             }
@@ -62,5 +66,44 @@ namespace Exercicio2
 
         }
 
+        public bool DataValida(int dia, int mes, int ano)
+        {
+            if (dia < 1 || dia > 31)
+            {
+                return false;
+            }
+
+            if (mes < 1 || mes > 12)
+            {
+                return false;
+            }
+
+            if (ano < 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool HoraValida(int hora, int minuto, int segundo)
+        {
+            if (hora < 0 || hora > 23)
+            {
+                return false;
+            }
+
+            if (minuto < 0 || minuto > 59)
+            {
+                return false;
+            }
+
+            if (segundo < 0 || segundo > 59)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
