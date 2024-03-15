c71da2c91814238e8cfa184cb51e7079c1f64f4a
albertolunia <albertolunia@gmail.com>
2024-03-15 10:04:05 -0300

    Exercicio 3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,36 @@
+﻿enum Exercicio
+{
+    Academia = 1,
+    Luta = 2,
+    Corrida = 3
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Opções de exercícios existentes:");
+        Console.WriteLine("1 - Academia");
+        Console.WriteLine("2 - Luta");
+        Console.WriteLine("3 - Corrida");
+
+        Console.Write("Digite o número do exercício desejado: ");
+        int exercicioEscolhido;
+        if (int.TryParse(Console.ReadLine(), out exercicioEscolhido))
+        {
+            if (Enum.IsDefined(typeof(Exercicio), exercicioEscolhido))
+            {
+                Exercicio exercicio = (Exercicio)exercicioEscolhido;
+                Console.WriteLine($"Exercício escolhido: {exercicio}");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida.");
+        }
+    }
+}
