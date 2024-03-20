9f0a001546e7512165fb703a6cb56e8d633a6c7c
albertolunia <albertolunia@gmail.com>
2024-03-20 08:11:06 -0300

    Exercicio 4 - Async/Await (completa)

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Task task1 = DoWorkAsync("Tarefa 1", 5, 1000);
+        Task task2 = DoWorkAsync("Tarefa 2", 7, 700);
+
+        await Task.WhenAll(task1, task2);
+
+        Console.WriteLine("Ambas as tarefas foram concluídas.");
+    }
+
+    static async Task DoWorkAsync(string taskName, int count, int delay)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"{taskName} está trabalhando...");
+            await Task.Delay(delay);
+        }
+    }
+}
